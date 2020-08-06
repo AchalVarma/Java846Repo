fa20454e506eae69d8c34831b8822abe8e5b752a
AchalVarma <62965934+AchalVarma@users.noreply.github.com>
2020-08-06 12:13:47 +0530

    Update EmployeeManagement.java

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -5,8 +5,10 @@ class Calculator{
   public void diff(int a, int b){
     System.out.println("Difference=" +(a-b));
   }
-  public void prod(int a, int b){
-    System.out.println("Product=" +(a*b));
+  \\\\\\\\\\\\\\\\\\\\\\\\\\\
+    ///////////////////////////
+    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+    ////////////////////////////////
 
   public void div(int a, int b){
     System.out.println("Quotient=" +(a/b));
